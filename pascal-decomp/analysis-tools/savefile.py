8402cc022714f4ba9c4b94741c23c0dfc36867a9
FyiurAmron <spamove@gmail.com>
2026-09-26 20:49:47 +0200

    refactor: drop obsolete one-off analysis scripts

--- pascal-decomp/analysis-tools/savefile.py
+++
@@ -1,4 +0,0 @@
-vals=open(r"E:\Develop\Reverse\bombki\PLIKI.TPU","rb").read().decode("cp437","replace").split()
-print("=== PLIKI.TPU numbered (80) ===")
-for i,v in enumerate(vals):
-    print("%3d: %s"%(i+1,v))
